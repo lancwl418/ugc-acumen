417126c5d9a8060f11d5c9b1f681fac98ed7ea92
User <user@Users-Mac-mini.lan>
2025-09-03 14:47:17 -0700

    missing controls

--- public/hashtag-ugc-masonry.js
+++ public/hashtag-ugc-masonry.js
@@ -268,7 +268,7 @@
       let mediaHtml = "";
       if (isVideo && isMp4) {
         mediaHtml = `
-          <video controlsclass="ugc-media-wrap" autoplay muted playsinline loop preload="metadata">
+          <video controls class="ugc-media-wrap" autoplay muted playsinline loop preload="metadata">
             <source src="${mediaUrl}" type="video/mp4" />
           </video>`;
       } else {
